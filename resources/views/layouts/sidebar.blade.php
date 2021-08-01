91aaa9afd30e70ce67b176409108c2c5ad7c479a
KahHin <chiahkahhin@hotmail.com>
2021-08-01 20:16:40 +0800

    Add in website name to the sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,22 +1,31 @@
 <div class="left-side-bar">
-	<div class="brand-logo">
-		<a href="index.html">
-			<img src="vendors/images/deskapp-logo.svg" alt="" class="dark-logo">
-			<img src="vendors/images/deskapp-logo-white.svg" alt="" class="light-logo">
+	{{-- <div class="brand-logo">
+		<a href="{{ route('home') }}">
+			<p style="font-size: 16px">EMS</p>
 		</a>
 		<div class="close-sidebar" data-toggle="left-sidebar-close">
 			<i class="ion-close-round"></i>
 		</div>
-	</div>
+	</div> --}}
+
 	<div class="menu-block customscroll">
 		<div class="sidebar-menu">
 			<ul id="accordion-menu">
+				<li>
+					<div class="dropdown-divider"></div>
+				</li>
+				<li>
+					<div class="sidebar-small-cap" style="text-align: center; padding: 0;">Emp. Management System</div>
+				</li>
+				<li>
+					<div class="dropdown-divider"></div>
+				</li>
 				<li class="dropdown">
 					<a href="javascript:;" class="dropdown-toggle">
 						<span class="micon dw dw-house-1"></span><span class="mtext">Home</span>
 					</a>
 					<ul class="submenu">
-						<li><a href="index.html">Dashboard style 1</a></li>
+						<li><a href="/">Dashboard style 1</a></li>
 						<li><a href="index2.html">Dashboard style 2</a></li>
 					</ul>
 				</li>
