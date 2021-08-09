feec8b32d8b66d1111e1705c82543d0d8646f593
KahHin <chiahkahhin@hotmail.com>
2021-08-09 19:47:40 +0800

    Create department table and department model

---
+++ database/migrations/2021_08_09_103442_create_departments_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Models\Department;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDepartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('departments', function (Blueprint $table) {
+            $table->id();
+            $table->string('departmentCode')->unique();
+            $table->string('departmentName')->unique();
+            $table->timestamps();
+        });
+
+        Department::create([
+            "departmentCode" => "D001",
+            "departmentName" => "Human Resource"
+        ]);
+        
+        Department::create([
+            "departmentCode" => "D002",
+            "departmentName" => "Finance"
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('departments');
+    }
+}
