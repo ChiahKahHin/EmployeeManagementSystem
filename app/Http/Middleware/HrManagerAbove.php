bb7179fc8ef17481c509f310fccab98d253f6df7
KahHin <chiahkahhin@hotmail.com>
2021-08-11 16:47:38 +0800

    Import Auth class

--- app/Http/Middleware/HrManagerAbove.php
+++ app/Http/Middleware/HrManagerAbove.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HrManagerAbove
 {
@@ -16,6 +17,11 @@ class HrManagerAbove
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        $user = Auth::user();
+
+        if ($user->isHrManager() || $user->isAdmin())
+            return $next($request);
+
+        return redirect()->route("login");
     }
 }
