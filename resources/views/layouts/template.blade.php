d11089866643431e37dd89d865b2dd8c8f00dc25
KahHin <chiahkahhin@hotmail.com>
2021-08-13 21:04:42 +0800

    Add in preloader

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -6,6 +6,17 @@
 		@include("layouts.head")
 	</head>
 	<body>
+		<div class="pre-loader">
+			<div class="pre-loader-box">
+				<div class='loader-progress' id="progress_div">
+					<div class='bar' id='bar1'></div>
+				</div>
+				<div class='percent' id='percent1'>0%</div>
+				<div class="loading-text">
+					System is loading...
+				</div>
+			</div>
+		</div>
 		@include("layouts.nav")
 
 		@include("layouts.sidebar")
