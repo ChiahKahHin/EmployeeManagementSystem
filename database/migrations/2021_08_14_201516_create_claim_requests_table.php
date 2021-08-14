48c9e74d21d34015ed3f74219e1e13c8586193c3
KahHin <chiahkahhin@hotmail.com>
2021-08-14 23:26:44 +0800

    Create claim request table

---
+++ database/migrations/2021_08_14_201516_create_claim_requests_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClaimRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('claim_requests', function (Blueprint $table) {
+            $table->id();
+            $table->integer('claimType');
+            $table->string('claimAmount');
+            $table->date('claimDate');
+            $table->text('claimDescription');
+            $table->integer('claimEmployee');
+            $table->integer('claimStatus');
+            $table->timestamps();
+        });
+
+        DB::statement("ALTER TABLE claim_requests ADD claimAttachment LONGBLOB");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('claim_requests');
+    }
+}
