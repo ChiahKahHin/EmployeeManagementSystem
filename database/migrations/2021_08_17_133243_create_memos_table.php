587d400694217e65be8001e161fd967925382efe
KahHin <chiahkahhin@hotmail.com>
2021-08-17 16:41:59 +0800

    Create memo table

---
+++ database/migrations/2021_08_17_133243_create_memos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMemosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('memos', function (Blueprint $table) {
+            $table->id();
+            $table->string('memoTitle');
+            $table->text('memoDescription');
+            $table->date('memoDate');
+            $table->integer('memoRecipient');
+            $table->dateTime('memoScheduled')->nullable();
+            $table->integer('memoStatus');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('memos');
+    }
+}
