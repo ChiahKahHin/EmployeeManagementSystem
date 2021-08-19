824bc2557a7b5eda365e09650452412d562d75ff
KahHin <chiahkahhin@hotmail.com>
2021-08-19 12:10:26 +0800

    Update view image in modal source code

--- resources/views/viewClaimRequest.blade.php
+++ resources/views/viewClaimRequest.blade.php
@@ -109,6 +109,7 @@
 @endsection
 
 @section("script")
+	<script src="{{ asset('vendors/scripts/image-modal.js') }}"></script>
 	<script>
 		$('#approveClaimRequest').on('click', function(){
 			swal({
@@ -183,26 +184,5 @@
 				reasonErrorMessage.innerHTML = "This field is required";
 			}
 		});
-
-		// Get the modal
-        var modal = document.getElementById("image-modal");
-        
-        // Get the image and insert it inside the modal - use its "alt" text as a caption
-        var img = document.getElementById("img-modal");
-        var modalImg = document.getElementById("img-block-preview");
-        var captionText = document.getElementById("caption");
-        img.onclick = function(){
-            modal.style.display = "block";
-            modalImg.src = this.src;
-            captionText.innerHTML = this.alt;
-        }
-        
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
-        
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() { 
-            modal.style.display = "none";
-        }
-		</script>
+	</script>
 @endsection
