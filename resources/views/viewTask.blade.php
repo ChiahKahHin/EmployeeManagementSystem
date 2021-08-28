9913a8757e76c9c2b316800f99bb17d2d1e4dc30
KahHin <chiahkahhin@hotmail.com>
2021-08-28 18:48:15 +0800

    Add in progress bar to indicate the task status
    Implement onclick modal on the progress bar

--- resources/views/viewTask.blade.php
+++ resources/views/viewTask.blade.php
@@ -9,6 +9,155 @@
 @endsection
 
 @section('content')
+	{{-- <div class="page-header">
+		<div class="row">
+			<div class="col-md-12 col-sm-12">
+				<div class="title">
+					<h4>Task Details</h4>
+				</div>
+				<nav aria-label="breadcrumb" role="navigation">
+					<ol class="breadcrumb">
+						<li class="breadcrumb-item"><a href="{{ route('manageTask') }}">Manage Task</a></li>
+						<li class="breadcrumb-item active" aria-current="page">View task details</li>
+					</ol>
+				</nav>
+				
+			</div>
+		</div>
+	</div> --}}
+	<div class="card">
+		<div class="row d-flex justify-content-between top-progressbar4">
+			<div class="d-flex pb-4">
+				<h5>Task Status</h5>
+			</div>
+		</div>
+		<div class="row d-flex justify-content-center">
+			<div class="col-12">
+				<ul id="progressbar4" class="text-center">
+					<li class="@if($task->status >= 0) active @endif step0" @if($task->status >= 0) style="cursor: pointer;" data-toggle="modal" data-target="#pending-modal" @endif></li>
+					<li class="@if($task->status >= 1) active @endif step0" @if($task->status >= 1) style="cursor: pointer;" data-toggle="modal" data-target="#waiting-approval-modal" @endif></li>
+					<li class="@if($task->status >= 2) active @endif step0" @if($task->status >= 2) style="cursor: pointer;" data-toggle="modal" data-target="#approved-rejected-modal" @endif></li>
+					<li class="@if($task->status >= 3) active @endif step0" @if($task->status >= 3) style="cursor: pointer;" data-toggle="modal" data-target="#completed-modal" @endif></li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="row justify-content-between top-progressbar4">
+			<div class="row d-flex icon-content">
+				<div class="d-flex flex-column">
+					<p class="font-weight-bold">Pending</p>
+				</div>
+			</div>
+			<div class="row d-flex icon-content">
+				<div class="d-flex flex-column">
+					<p class="font-weight-bold">Waiting Approval</p>
+				</div>
+			</div>
+			<div class="row d-flex icon-content">
+				<div class="d-flex flex-column">
+					<p class="font-weight-bold">@if($task->status < 2) Approved/Rejected @elseif($task->status == 2) Rejected @elseif($task->status == 3) Approved @endif</p>
+				</div>
+			</div>
+			<div class="row d-flex icon-content">
+				<div class="d-flex flex-column">
+					<p class="font-weight-bold">Completed<br></p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	{{-- Task Modals --}}
+	<div class="modal fade" id="pending-modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+		<div class="modal-dialog modal-dialog-centered">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h4 class="modal-title" id="myLargeModalLabel">Task Pending</h4>
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+				</div>
+				<div class="modal-body">
+					<p class="text-justify">
+						Once the task is completed, kindly click on the complete task button below. <br>
+						An email notification will be sent for the manager to approve/reject the task. <br>
+					</p>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="modal fade" id="waiting-approval-modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+		<div class="modal-dialog modal-dialog-centered">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h4 class="modal-title" id="myLargeModalLabel">Task Waiting Approval</h4>
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+				</div>
+				<div class="modal-body">
+					<p class="text-justify">
+						This task is waiting for the manager approval. <br>
+						An email notification will be sent for the employee once manager approve/reject it.
+					</p>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="modal fade" id="approved-rejected-modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+		<div class="modal-dialog modal-dialog-centered">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h4 class="modal-title" id="myLargeModalLabel">
+						@if ($task->status == 2)
+							Task Rejected
+						@elseif ($task->status == 3)
+							Task Approved
+						@endif
+					</h4>
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+				</div>
+				<div class="modal-body">
+					<p class="text-justify">
+						@if ($task->status == 2)
+							This task is rejected by the manager. <br>
+							Kindly refer to the reason given by the manager and make the changes. <br>
+							Once changes is made, you can request for approval again.
+						@elseif ($task->status == 3)
+							This task is approved by the manager.
+						@endif
+					</p>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="modal fade" id="completed-modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+		<div class="modal-dialog modal-dialog-centered">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h4 class="modal-title" id="myLargeModalLabel">Task Completed</h4>
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+				</div>
+				<div class="modal-body">
+					<p class="text-justify">
+						This task is completed.
+					</p>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	{{-- Task Details Tables --}}
 	<div class="pd-20 card-box mb-30">
 		<div class="clearfix mb-20">
 			<div class="pull-left">
@@ -31,26 +180,14 @@
 					<td class="font-weight-bold">Description</td>
 					<td>{{ ucfirst($task->description) }}</td>
 				</tr>
-				@if (Auth::user()->isEmployee())
-					<tr>
-						<td class="font-weight-bold">Manager</td>
-						<td>{{ $task->getManager->getFullName() }}</td>
-					</tr>
-				@elseif (Auth::user()->isHrManager() || Auth::user()->isManager())
-					<tr>
-						<td class="font-weight-bold">Person In Charge</td>
-						<td>{{ $task->getPersonInCharge->getFullName() }}</td>
-					</tr>
-				@else
-					<tr>
-						<td class="font-weight-bold">Manager</td>
-						<td>{{ $task->getManager->getFullName() }}</td>
-					</tr>
-					<tr>
-						<td class="font-weight-bold">Person In Charge</td>
-						<td>{{ $task->getPersonInCharge->getFullName() }}</td>
-					</tr>
-				@endif
+				<tr>
+					<td class="font-weight-bold">Manager</td>
+					<td>{{ $task->getManager->getFullName() }}</td>
+				</tr>
+				<tr>
+					<td class="font-weight-bold">Person In Charge</td>
+					<td>{{ $task->getPersonInCharge->getFullName() }}</td>
+				</tr>
 				<tr>
 					<td class="font-weight-bold">Priority</td>
 					<td>{{ $task->priority }}</td>
@@ -63,6 +200,42 @@
 					<td class="font-weight-bold">Status</td>
 					<td>{{ $task->getStatus() }}</td>
 				</tr>
+				@if (count($task->getRejectedReasons) > 0)
+					<tr>
+						<td class="font-weight-bold">Number of times task being rejected</td>
+						<td>{{ count($task->getRejectedReasons) }}</td>
+					</tr>
+					<tr>
+						<td class="font-weight-bold">Task Rejected Reasons (Date & Time)</td>
+						<td>
+							<ol>
+								@foreach ($task->getRejectedReasons as $rejectedReason)
+									<li>
+										{{ $loop->iteration }}. {{ $rejectedReason->rejectedReason }} ({{ date("d F Y, g:ia", strtotime($rejectedReason->created_at)) }})
+									</li>
+								@endforeach
+							</ol>
+						</td>
+					</tr>
+				@endif
+				{{-- @if (count($rejectedReasons) > 0)
+					<tr>
+						<td class="font-weight-bold">Number of times task being rejected</td>
+						<td>{{ count($rejectedReasons) }}</td>
+					</tr>
+					<tr>
+						<td class="font-weight-bold">Task Rejected Reasons (Date & Time)</td>
+						<td>
+							<ol>
+								@foreach ($rejectedReasons as $rejectedReason)
+									<li>
+										{{ $loop->iteration }}. {{ $rejectedReason->rejectedReason }} ({{ date("d F Y, g:ia", strtotime($rejectedReason->created_at)) }})
+									</li>
+								@endforeach
+							</ol>
+						</td>
+					</tr>
+				@endif --}}
 				<tr>
 					<td class="font-weight-bold">Task Created Date & Time</td>
 					<td>{{ date("d F Y, g:ia", strtotime($task->created_at)) }}</td>
